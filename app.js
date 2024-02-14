d12024ad98be22330174c73fd4ea5efa72a88c13
Jessica <jessicaoliveira.dev1@gmail.com>
2024-02-13 23:21:16 -0300

    config database

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const mysql = require('mysql');
+const config = require('./config');
+
 const app = express();
 const port = 3000;
 
+const connection = mysql.createConnection({
+  host: config.host,
+  user: config.user,
+  password: config.password,
+  database: config.database
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+    return;
+  }
+  console.log('Conexão bem-sucedida ao banco de dados!');
+});
+
 app.get('/', (req, res) => {
-  res.send('hghr!');
+  res.send('Oi!');
 });
 
 app.listen(port, () => {
